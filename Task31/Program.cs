63266b9d42ce52355cb14bcef56a225c82c25228
Наталья Ярцева <zabaluna@mail.ru>
2022-11-13 00:45:06 +0300

    Commit

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -14,18 +14,18 @@ int[] CreateRandomArray(int N, int start, int end)
     return RandomArray;
 }
 
-int[] CreateArray()
-{
-    Console.WriteLine("Введите количество элементов массива");
-    int size = Convert.ToInt32(Console.ReadLine());
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
-    {   
-        Console.WriteLine($"Введите {i+1} элемент массива");
-        array[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    return array;
-}
+// int[] CreateArray()
+// {
+//     Console.WriteLine("Введите количество элементов массива");
+//     int size = Convert.ToInt32(Console.ReadLine());
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {   
+//         Console.WriteLine($"Введите {i+1} элемент массива");
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
 
 void ShowArray(int[] array)
 {

b315caf90f5d76574457253f20a90dfc22ddebd4
Наталья Ярцева <zabaluna@mail.ru>
2022-11-13 01:30:08 +0300

    Count %2==0 in Array

---
+++ Task34HW/Program.cs
@@ -0,0 +1,34 @@
+﻿//  Задача 34: Задайте массив заполненный
+//   случайными положительными трёхзначными числами.
+//   Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(10, -100, 1000);
+ShowArray(myArray);
+
+int count = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 0)
+    count++;
+}
+Console.WriteLine($"{count}: четные числа в массиве");

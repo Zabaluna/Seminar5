af678831c75ac0fe72fed338d78f323912bdf3a2
Наталья Ярцева <zabaluna@mail.ru>
2022-11-13 00:59:34 +0300

    Multiplication 1st and last numers,2d and last-1 etc

---
+++ Task37/Program.cs
@@ -0,0 +1,36 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+//  Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(5, 1, 11);
+ShowArray(myArray);
+
+for (int i = 0; i < myArray.Length/2; i++)
+{
+    Console.Write(myArray[i]*myArray[myArray.Length-i-1]+" ");
+}
+if (myArray.Length%2!=0)
+{
+Console.Write(myArray[myArray.Length/2]+" ");
+}

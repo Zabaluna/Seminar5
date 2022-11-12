020edf51e4bc0a6ff38e6544cc203183b893c167
Наталья Ярцева <zabaluna@mail.ru>
2022-11-12 23:59:28 +0300

    Plus and minus numbers changed

---
+++ Task32/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените 
+// на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] myArray = CreateRandomArray(10, -100, 100);
+ShowArray(myArray);
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = myArray[i]* (-1);
+    Console.Write($"{myArray[i]} ");
+}
+
